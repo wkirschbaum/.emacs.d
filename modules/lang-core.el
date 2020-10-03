c0f2d03a1898565c401ba1bca7f733ee3c86ca72
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-10-03 16:04:59 +0200

    Update lsp-mode config

--- modules/lang-core.el
+++ modules/lang-core.el
@@ -7,16 +7,41 @@
             ;; if you want which-key integration
             (lsp-mode . lsp-enable-which-key-integration))
     :commands lsp
+    :init
+    (add-to-list `exec-path "~/src/tools/elixir-ls/bin/")
     :config
-    (setq lsp-clients-elixir-server-executable "~/src/tools/elixir-ls/bin/language_server.sh")
-    (setq lsp-enable-file-watchers t)
-    (setq lsp-file-watch-threshold 8000))
+    (setq lsp-auto-guess-root t
+          lsp-enable-file-watchers t
+          lsp-file-watch-threshold 8000
+          lsp-file-watch-ignored
+          '("[/\\\\]\\.git$"
+            "[/\\\\]\\.elixir_ls$"
+            "[/\\\\]_build$"
+            "[/\\\\]assets$"
+            "[/\\\\]cover$"
+            "[/\\\\]node_modules$"
+            "[/\\\\]submodules$"
+          )))
 
 ;; optionally
 (use-package lsp-ui
   :ensure t
   :commands lsp-ui-mode
-  :bind (("M-i" . lsp-ui-imenu)))
+  :bind (("M-i" . lsp-ui-imenu))
+  :config
+  (setq lsp-ui-doc-enable nil
+        lsp-ui-doc-use-webkit t
+        lsp-ui-peek-enable nil))
+
+(use-package company-lsp
+  :commands company-lsp
+  :ensure t
+  :after (company lsp)
+  :config
+  (setq company-transformers nil
+        company-lsp-async t
+        company-lsp-cache-candidates nil)
+  (push 'company-lsp company-backends))
 
 ;; if you are helm user
 (use-package helm-lsp :commands helm-lsp-workspace-symbol)
@@ -25,3 +50,9 @@
 (use-package which-key
   :config
   (which-key-mode))
+
+;; (use-package dap-mode
+;;   :ensure t
+;;   :config
+;;   (require 'dap-elixir)
+;;   (setq dap-auto-configure-features '(sessions locals controls tooltip)))
