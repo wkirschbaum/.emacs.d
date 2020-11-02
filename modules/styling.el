0e5e6e7a11b26c371c38e4480fa5ed195fcd0735
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2020-11-02 13:22:48 +0200

    ...

--- modules/styling.el
+++ modules/styling.el
@@ -5,13 +5,18 @@
         minions-direct '(projectile-mode flycheck-mode))
   (minions-mode 1))
 
-(use-package solarized-theme
+(use-package color-theme-sanityinc-tomorrow
   :ensure t
   :config
-  (load-theme 'solarized-dark t)
-  (let ((line (face-attribute 'mode-line :underline)))
-    (set-face-attribute 'mode-line          nil :overline   line)
-    (set-face-attribute 'mode-line-inactive nil :overline   line)
-    (set-face-attribute 'mode-line-inactive nil :underline  line)
-    (set-face-attribute 'mode-line          nil :box        nil)
-    (set-face-attribute 'mode-line-inactive nil :box        nil)))
+  (color-theme-sanityinc-tomorrow-eighties))
+
+;; (use-package solarized-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'solarized-dark t)
+;;   (let ((line (face-attribute 'mode-line :underline)))
+;;     (set-face-attribute 'mode-line          nil :overline   line)
+;;     (set-face-attribute 'mode-line-inactive nil :overline   line)
+;;     (set-face-attribute 'mode-line-inactive nil :underline  line)
+;;     (set-face-attribute 'mode-line          nil :box        nil)
+;;     (set-face-attribute 'mode-line-inactive nil :box        nil)))
