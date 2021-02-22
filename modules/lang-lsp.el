0f48a2391bfb922fecdc1fafce97b247086e56e1
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-02-22 20:35:08 +0200

    Fix lsp-mode config

--- modules/lang-lsp.el
+++ modules/lang-lsp.el
@@ -1,25 +1,25 @@
 (use-package lsp-mode
   :ensure t
+  :init
+   (setq lsp-keymap-prefix "C-c l")
+   (setq lsp-enable-file-watchers t
+         lsp-file-watch-threshold 8000
+         lsp-modeline-code-actions-enable t
+         lsp-file-watch-ignored-directories
+         '("[/\\\\]\\.git$"
+           "[/\\\\]\\.elixir_ls$"
+           "[/\\\\]_build$"
+           "[/\\\\]deps"
+           "[/\\\\]assets$"
+           "[/\\\\]cover$"
+           "[/\\\\]node_modules$"
+           "[/\\\\]submodules$"
+           ))
   :hook ((elixir-mode . lsp)
          (rust-mode . lsp))
   :commands lsp
   :init
-  (add-to-list `exec-path "~/src/tools/elixir-ls/bin/")
-  :config
-  (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-enable-file-watchers t
-        lsp-file-watch-threshold 8000
-        lsp-modeline-code-actions-enable t
-        lsp-file-watch-ignored-directories
-        '("[/\\\\]\\.git$"
-          "[/\\\\]\\.elixir_ls$"
-          "[/\\\\]_build$"
-          "[/\\\\]deps"
-          "[/\\\\]assets$"
-          "[/\\\\]cover$"
-          "[/\\\\]node_modules$"
-          "[/\\\\]submodules$"
-          )))
+  (add-to-list `exec-path "~/src/tools/elixir-ls/bin/"))
 
 (use-package lsp-ui
   :ensure t
