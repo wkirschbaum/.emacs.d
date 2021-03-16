35e36bda1aa48313eb895e95a1459015a5a83789
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-03-16 09:33:49 +0200

    Try deft etc..

--- modules/org.el
+++ modules/org.el
@@ -1,13 +1,6 @@
 (use-package org
   :config
-  (setq org-agenda-files (file-expand-wildcards "~/Cloud/Org/*.org"))
-  (setq org-directory "~/Cloud/Org/")
-  (setq org-capture-templates
-        '(("n" "Note" entry (file+headline "~/Cloud/Org/notes.org" "Notes") "* %?\n%U\n%a")
-          ("c" "Code" entry (file+headline "~/Cloud/Org/codes.org" "Unsorted") "* %?\n  %a")
-          ("p" "People" entry (file+headline "~/Cloud/Org/people.org" "Unsorted") "* %?\n%U\n")
-          ("a" "Appointment" entry (file+headline "~/Cloud/Org/calendar.org" "Appointments") "* %?\n")
-          ("t" "Todo" entry (file+headline "~/Cloud/Org/todo.org" "Today") "* TODO %?")))
+  (setq org-agenda-files (file-expand-wildcards "~/Cloud/notes/*.org"))
 
   (setq-default org-todo-keywords
                 '((sequence "TODO(t)" "DOING(b)" "|" "DONE(d)")))
@@ -30,13 +23,58 @@
   (add-hook 'org-mode-hook #'toggle-word-wrap)
   (add-hook 'text-mode-hook #'toggle-word-wrap)
 
+  ;; (org-indent-mode)
+  ;; (setq org-hide-emphasis-markers t)
+
+  ;; (font-lock-add-keywords 'org-mode
+  ;;                         '(("^ *\\([-]\\) "
+  ;;                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; (let* ((variable-tuple
+  ;;         (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
+  ;;               ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+  ;;               ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+  ;;               ((x-list-fonts "Verdana")         '(:font "Verdana"))
+  ;;               ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+  ;;               (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+  ;;        (base-font-color     (face-foreground 'default nil 'default))
+  ;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  ;;   (custom-theme-set-faces
+  ;;    'user
+  ;;    `(org-level-8 ((t (,@headline ,@variable-tuple))))
+  ;;    `(org-level-7 ((t (,@headline ,@variable-tuple))))
+  ;;    `(org-level-6 ((t (,@headline ,@variable-tuple))))
+  ;;    `(org-level-5 ((t (,@headline ,@variable-tuple))))
+  ;;    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+  ;;    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.20))))
+  ;;    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.30))))
+  ;;    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.40))))
+  ;;    `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+  ;; (custom-theme-set-faces
+  ;;  'user
+  ;;  '(org-block ((t (:inherit fixed-pitch))))
+  ;;  '(org-code ((t (:inherit (shadow fixed-pitch)))))
+  ;;  '(org-document-info ((t (:foreground "dark orange"))))
+  ;;  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+  ;;  '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+  ;;  '(org-link ((t (:foreground "royal blue" :underline t))))
+  ;;  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+  ;;  '(org-property-value ((t (:inherit fixed-pitch))) t)
+  ;;  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+  ;;  '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+  ;;  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+  ;;  '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+
   ;; Calendar and Diary
   (setq calendar-view-diary-initially-flag t
         calendar-mark-diary-entries-flag t
         european-calendar-style 't
-        diary-file "~/Cloud/Org/diary")
+        diary-file "~/Cloud/notes/diary")
 
   (setq org-catch-invisible-edits 'error))
+
 
 ;; Persist org-clock between sessions
 (setq org-clock-persist 'history)
@@ -44,10 +82,18 @@
 (setq org-clock-idle-time 15)
 
 (setq org-ellipsis " ↴")
-;; (setq org-src-tab-acts-natively t)
 
 (use-package which-key
   :ensure t)
+
+(use-package deft
+  :ensure t
+  :bind ("C-c n" . deft)
+  :bind ("C-c l" . deft-find-file)
+  :config
+  (setq deft-recursive t
+        deft-extensions '("txt" "org" "md")
+        deft-directory "~/Cloud/notes"))
 
 (use-package org-roam
   :ensure t
@@ -55,10 +101,12 @@
   :bind ("C-c M-c" . org-roam-capture)
   :bind ("C-c r f" . org-roam-find-file)
   :bind ("C-c r r" . org-roam)
+  :bind ("C-c r i" . org-roam-insert)
   :config
   (add-hook 'after-init-hook 'org-roam-mode)
   (setq org-roam-dailies-directory "daily/")
-  (setq org-roam-directory "~/Cloud/org-roam")
+  (setq org-roam-directory "~/Cloud/notes")
+  (setq org-roam-db-update-method 'immediate)
   (setq org-roam-dailies-capture-templates
         '(("d" "random" entry
            #'org-roam-capture--get-point
